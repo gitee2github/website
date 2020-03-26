ef6e5b8b66000fd4bd7725e473e4682bb263dc39
freesky-edward <freesky.edward@gmail.com>
2020-03-26 21:12:05 +0800

    feature: generate docs menu automatically

--- script/docs-builder.sh
+++ script/docs-builder.sh
@@ -9,6 +9,9 @@ git clone https://gitee.com/openeuler/docs
 
 cd ./docs
 
+fd=$(dirname $0)
+pd=$(dirname "$fd") 
+
 for r in $(git branch -r --list "origin/stable-*")
 do
   b=${r##*origin/stable-}
@@ -19,6 +22,13 @@ do
  
   mkdir -p ../content/en/docs/$b
   cp -r ./content/en/* ../content/en/docs/$b/
+
+  cd ..
+
+  sed -e "s/{{version}}/$b/g" $fd/menu.temp >> $pd/config.toml 
+
+  cd ./docs
+  
 done
 
 cd ..
